8f957772cbcd21d64534ef22b1abab1f4930b8cb
tejaswi <tejaswikondalu@gmail.com>
2021-03-04 13:21:36 +0530

    added comment at last line

--- script.sh
+++ script.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-for i in {1..100}
-do
-if [ $(expr $i % 2) -eq 0 ]
-then
-echo "$i is Even Number"
-else
-echo "$i is Odd Number"
-fi
+for i in {1..100}; do
+    if [ $(expr $i % 2) -eq 0 ]; then
+        echo "$i is Even Number"
+    else
+        echo "$i is Odd Number"
+    fi
 done
-
+#End of the Script...!!!
